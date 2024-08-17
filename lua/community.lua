1c08b98d226155e0bab6ac61f5b48a3d440fd1c5
Artem Kalachev <artem.kalachev.2002@gmail.com>
2024-08-17 08:40:56 +0500

    fix: dart

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,3 @@
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,5 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.dart" },
   -- import/override with your plugins folder
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -10,6 +8,16 @@ return {
       "lua",
       "vim",
       -- add more arguments for adding more treesitter parsers
+    },
+    textobjects = {
+      select = {
+        enable = false,
+      },
+    },
+
+    indent = {
+      enable = true,
+      disable = { "dart" },
     },
   },
 }
